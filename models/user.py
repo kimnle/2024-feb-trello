52ecedf5a480724d6129f88d4cb42b6936b97afc
kimnle <kimnle002@gmail.com>
2024-06-20 20:28:32 +1000

    Add card model, and relate user and card

--- models/user.py
+++ models/user.py
@@ -1,4 +1,5 @@
 from init import db, ma
+from marshmallow import fields
 
 class User(db.Model):
     # name of the table
@@ -11,7 +12,12 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
+    cards = db.relationship("Card", back_populates="user")
+
 class UserSchema(ma.Schema):
+
+    cards = fields.List(fields.nested("CardSchema", exclude=["user"]))
+
     class Meta:
         fields = ("id", "name", "email", "password", "is_admin")
 
